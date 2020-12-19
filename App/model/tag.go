c7640c4aefac862dcafd0d850030e4a2ee03debe
HHxx66 <5521962+HHxx66@user.noreply.gitee.com>
2020-12-20 00:40:24 +0800

    user

--- App/model/tag.go
+++ App/model/tag.go
@@ -7,8 +7,8 @@ import (
 type Tag struct {
 
 	// The ID of the Blog
-	BlogID primitive.ObjectID `json:"BlogID,omitempty"`
+	BlogID primitive.ObjectID `json:"BlogID,omitempty" bson:"BlogID"`
 
 	// all of the tags
-	Content string `json:"content,omitempty"`
+	Content string `json:"content,omitempty" bson:"content"`
 }
