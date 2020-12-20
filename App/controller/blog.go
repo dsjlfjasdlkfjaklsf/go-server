8faf86c009a2d7de9b72fccf6e5f6de6c00d5535
HHxx66 <5521962+HHxx66@user.noreply.gitee.com>
2020-12-20 13:18:04 +0800

    finish blog

--- App/controller/blog.go
+++ App/controller/blog.go
@@ -3,29 +3,66 @@ package controller
 import (
 	// "github.com/dsjlfjasdlkfjaklsf/go-server/App/model"
 	"net/http"
+
+	"github.com/dsjlfjasdlkfjaklsf/go-server/App/model"
+	"github.com/dsjlfjasdlkfjaklsf/go-server/App/util"
 )
 
 func AddBlog(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	handler := util.CreateHandler(w, r)
+	body := model.RegistBody{}
+	err := handler.DecodePost(&body)
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	_, err = Service.Blog.AddBlog(body)
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	handler.Send("success", true)
 }
 
 func DeleteBlog(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	handler := util.CreateHandler(w, r)
+	id := handler.DecodePath(2)
+	err = Service.Blog.DeleteBlog(id)
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	handler.Send("success", true)
 }
 
 func GetBlogByBlogID(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	handler := util.CreateHandler(w, r)
+	id := handler.DecodePath(2)
+	blog, err := Service.Blog.GetBlogByBlogID(id)
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	handler.Send(blog, true)
 }
 
 func GetBlogs(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	handler := util.CreateHandler(w, r)
+	blogs, err := Service.Blog.GetBlogs()
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	handler.Send(blogs, true)
 }
 
 func GetBlogsByID(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	handler := util.CreateHandler(w, r)
+	id := handler.DecodePath(2)
+	blogs, err := Service.Blog.GetBlogsByID(id)
+	if err != nil {
+		handler.Send(err.Error(), false)
+		return
+	}
+	handler.Send(blogs, true)
 }
